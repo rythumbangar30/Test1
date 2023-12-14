15a1c60e3ee28c4a83743fe2c924991e1d727689
Rythum Bangar <shobhit.dubey@liseinfotech.com>
2023-12-15 02:43:03 +0530

    Code Added

---
+++ src/main/java/org/example/Exception1.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Exception1 {
+    public static void main(String[] args) {
+
+    String s1=null;
+    int d[]={1,2,3};
+    try{
+        System.out.println(s1.length());
+    }catch(NullPointerException e){
+        System.out.println("File some words in an String");
+    }
+    try{
+        System.out.println(d[3]);
+    }catch(ArrayIndexOutOfBoundsException e){
+        System.out.println("Index of array is invalid");
+    }
+    System.out.println("Bye");
+    }
+}
